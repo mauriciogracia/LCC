66f517c3f805cadc194526a2756a0a9f860cd76c
Mauricio Gracia G <mauriciogracia@gmail.com>
2025-10-30 08:29:04 -0500

    create Referral controller and first method exposed in swagger

---
+++ Controllers/ReferralsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LCC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReferralsController : ControllerBase
+    {
+        /// <summary>
+        /// referralCode = GetUniqueReferralCode (uid) 
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <returns></returns>
+        [HttpGet("GetReferralCode")]
+        public string GetReferralCode(string uid)
+        {
+            string code = string.Empty;
+
+            return code;
+        }
+    }
+    
+
+    
+
+}
