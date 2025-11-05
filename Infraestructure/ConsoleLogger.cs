08850ce78be409e5268f9cccaf950703f342344d
Mauricio Gracia G <mauriciogracia@gmail.com>
2025-11-05 08:17:30 -0500

    INFRA initial layer

---
+++ Infraestructure/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+﻿
+using Domain;
+
+namespace Infrastructure
+{
+    public class ConsoleLogger : ILog
+    {
+        public void error(string message)
+        {
+            Console.Error.WriteLine(message);
+        }
+
+        public void info(string message)
+        {
+            Console.Out.WriteLine(message);
+        }
+    }
+}
